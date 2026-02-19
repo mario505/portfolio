e87d80f4aedf9a92b03808c09b1bcb3d761e3306
mario <mario_reyes505@hotmail.com>
2026-02-18 22:07:25 -0500

    create lib/utils.js add func cn to combine tailwind classes, create ThemeToggle.jsx to switch between light and night mode, add ThemeToggle to Home.jsx, fix typos on index.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import ThemeToggle from "../components/ThemeToggle";
+
 export default function Home(){
     return (
-    <div>
+    <div className="min-h-screen bg-backround text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
+        <ThemeToggle />
+
 
         {/* Background Effects */}
 
