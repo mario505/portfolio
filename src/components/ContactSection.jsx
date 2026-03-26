e5ae63946e1db9a8eff76234f43dd2070572e0d8
mario <mario_reyes505@hotmail.com>
2026-03-25 23:10:48 -0500

    update vite.config for deployment. Update project images. Refactor formatting and remove contact form on ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,129 +1,80 @@
-import { Instagram, Linkedin, Mail, Phone, Send, Twitter } from "lucide-react"
-import { cn } from "../lib/utils"
+import { Instagram, Linkedin, Mail, Phone, Send, Twitter } from "lucide-react";
+import { cn } from "../lib/utils";
 
 export default function ContactSection() {
-    return (
-        <section id="contact" className="py-24 px-4 relative bg-secondary/30">
-            <div className="container mx-auto max-w-5xl">
-                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-                    Get in <span className="text-primary">Touch</span>
-                </h2>
+  return (
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Get in <span className="text-primary">Touch</span>
+        </h2>
 
-                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-                    Have a project in mind or want to collaborate? Feel free to reach out. 
-                    I'm always open to discussing new opportunities.
-                </p>
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Have a project in mind or want to collaborate? Feel free to reach out.
+          I'm always open to discussing new opportunities.
+        </p>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-                    <div className="space-y-8">
-                        <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+        <div className="flex items-center justify-center">
+          <div className="flex-col items-center space-y-8 text-center" >
+            <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
 
-                        <div className="space-y-6 justify-center">
-                            <div className="flex items-start space-x-4">
-                                <div className="p-3 rounded-full bg-primary/10">
-                                    <Mail className="h-6 w-6 text-primary"/>
-                                </div>
-                                <div>
-                                    <h4 className="font-medium">Email</h4>
-                                    <a href="mail:hellowordl@hgmail.com"
-                                        className={cn("text-muted-foreground hover:text-primary",
-                                            "transitions-colors"
-                                        )}>
-                                            Mario-505@gmail
-                                        </a>
-                                </div>
-                            </div>
-
-                            <div className="flex items-start space-x-4">
-                                <div className="p-3 rounded-full bg-primary/10">
-                                    <Phone className="h-6 w-6 text-primary"/>
-                                </div>
-                                <div>
-                                    <h4 className="font-medium">Phone</h4>
-                                    <a href="tel: 12345311"
-                                        className={cn("text-muted-foreground hover:text-primary",
-                                            "transitions-colors"
-                                        )}>
-                                            +51 980757083
-                                        </a>
-                                </div>
-                            </div>
-
-                            <div className="flex items-start space-x-4">
-                                <div className="p-3 rounded-full bg-primary/10">
-                                    <Mail className="h-6 w-6 text-primary"/>
-                                </div>
-                                <div>
-                                    <h4 className="font-medium">Location</h4>
-                                    <a className={cn("text-muted-foreground hover:text-primary",
-                                            "transitions-colors"
-                                        )}>
-                                            Lima, Peru (South America)
-                                        </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="pt-8">
-                            <h4 className="font-medium 4-mb">Connect With Me</h4>
-                            <div className="flex space-x-4 justify-center">
-                                <a href="#" target="_blank">
-                                    <Linkedin />
-                                </a>
-                                <a href="#" target="_blank">
-                                    <Twitter />
-                                </a>
-                                <a href="#" target="_blank">
-                                    <Instagram />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="bg-card p-8 rounded-lg shadow-xs">
-                        <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
-
-                        <form className="space-y-6">
-                            <div>
-                                <label htmlFor="name" className="block text-sm font-medium mb-2">
-                                    Your Name
-                                </label>
-                                <input type="text" id="name" name="name" required
-                                    className={cn("w-full px-4 py-3 rounded-md border border-input",
-                                        "bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                                    )} placeholder="Mario Reyes..."/>
-                            </div>
-
-                            <div>
-                                <label htmlFor="email" className="block text-sm font-medium mb-2">
-                                    Your Email
-                                </label>
-                                <input type="email" id="email" name="email" required
-                                    className={cn("w-full px-4 py-3 rounded-md border border-input",
-                                        "bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                                    )} placeholder="johnDoe@gmail.com"/>
-                            </div>
-
-                            <div>
-                                <label htmlFor="message" className="block text-sm font-medium mb-2">
-                                    Your Message
-                                </label>
-                                <textarea id="message" name="message" required
-                                    className={cn("w-full px-4 py-3 rounded-md border border-input resize-none",
-                                        "bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                                    )} placeholder="Hello, I'd like to talk about..."/>
-                            </div>
-
-                            <button type="submit" className={cn("cosmic-button w-full flex items-center",
-                                "justify-center gap-2",
-                                {}
-                            )}>
-                                <Send size={16}/>
-                            </button>
-                        </form>
-                    </div>
+            <div className="flex items-center space-y-6 justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />
                 </div>
+                <div>
+                  <h4 className="font-medium">Email</h4>
+                  <a
+                    href="mail:hellowordl@hgmail.com"
+                    className={cn(
+                      "text-muted-foreground hover:text-primary",
+                      "transitions-colors",
+                    )}
+                  >
+                    Mario-505@gmail
+                  </a>
+                </div>
+              </div>
+
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Phone</h4>
+                  <a
+                    href="tel: 12345311"
+                    className={cn(
+                      "text-muted-foreground hover:text-primary",
+                      "transitions-colors",
+                    )}
+                  >
+                    +51 980757083
+                  </a>
+                </div>
+              </div>
+
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Location</h4>
+                  <a
+                    className={cn(
+                      "text-muted-foreground hover:text-primary",
+                      "transitions-colors",
+                    )}
+                  >
+                    Lima, Peru (South America)
+                  </a>
+                </div>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
