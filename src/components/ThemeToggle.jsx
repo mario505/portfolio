3f5a04fe7e6d9c0c3ad5c52e141a1cd1fa70ada6
mario <mario_reyes505@hotmail.com>
2026-03-29 09:59:08 -0500

    Set night mode as default, updated resume document

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { cn } from "../lib/utils";
 
 export default function ThemeToggle() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
     useEffect(()=> {
         const storedTheme = localStorage.getItem("theme");
